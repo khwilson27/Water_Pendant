619ab4de06495f5dd05d3c81d6644a46c7532cec
Kevin Wilson <Kevin Wilson>
2017-04-14 22:11:08 -0700

    print Char on Grid working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,31 +10,39 @@ $(document).ready(function() {
                   [0, 0, 0, 0, 0, 0],
                   [0, 0, 1, 1, 0, 0] ];
 
-  var yourChar1Location = [3,0];
-  var yourChar2Location = [4,0];
+  var yourChar1Location = [3,1];
+  var yourChar2Location = [4,1];
   var enemyChar1Location = [3,8];
   var enemyChar2Location = [4,8];
 
-  var charImages = ["","","",""]
+  var charLocations = [yourChar1Location, yourChar2Location, enemyChar1Location, enemyChar2Location];
+  var charImagesUrl = ["assets/images/character1.jpg", "assets/images/character2.jpg", "assets/images/character3.jpg", "assets/images/character4.png"];
 
 
+  var printCharOnGrid = function() {
 
-  var printCharOnGrid = function {
-
-        var charLocations = [yourChar1Location, yourChar2Location, enemyChar1Location, enemyChar2Location];
         for (var i=0; i<charLocations.length; i++) {
             var charX = charLocations[i][0];
             var charY = charLocations[i][1];
-            $("#x"+charX+"y"+charY).html("")
+            var iPlus1 = i + 1;
+
+            console.log("iteration " + i +" X: " + charX);
+            console.log("iteration " + i +" Y: " + charY);
+
+            var newImg = $("<img>").addClass("charOnGrid center-block").attr("src", charImagesUrl[i]).attr("id", "charImg"+iPlus1);
+
+            $("#x" + charX + "y" + charY).html(newImg);
         }
-
-
   }
 
+  printCharOnGrid();
 
 
 
 
-})
+
+
+
+}) // closes ready function
 
 
